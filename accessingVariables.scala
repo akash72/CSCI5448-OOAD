8e9c63510952c5ec26a2acbd6f32887909514164
SanandaB <34948941+SanandaB@users.noreply.github.com>
2019-04-14 22:35:49 -0600

    Add files via upload

---
+++ accessingVariables.scala
@@ -0,0 +1,10 @@
+﻿class Student{  
+    var id:Int = 100;                         // All fields must be initialized  
+    var name:String = "Smith";  
+}  
+object MainObject{  
+    def main(args:Array[String]){  
+        var s = new Student()               // Creating an object  
+        println(s.id+" "+s.name);  
+    }  
+}  
---
+++ polymorphism_methodOverloading.scala
@@ -0,0 +1,18 @@
+﻿class Arithmetic{  
+    def add(a:Int, b:Int){  
+        var sum = a+b  
+        println(sum)  
+    }  
+    def add(a:Double, b:Double){  
+        var sum = a+b  
+        println(sum)  
+    }  
+}  
+object MainObject{  
+    def main(args:Array[String]){  
+        var b = new Arithmetic()  
+        b.add(10,10)  
+        b.add(10.0,20.0)  
+  
+    }  
+}  
